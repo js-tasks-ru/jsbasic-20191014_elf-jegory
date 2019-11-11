8714c534b834726480b7fa980b8f11e7131cb55c
Egor Nazarovich <elf-jegory@yandex.ru>
2019-11-11 13:42:18 +0500

    module-3-1

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,7 +1,29 @@
-/**
- * Генерация HTML списка друзей
- * @param {Object[]} friends
- * @return {HTMLUListElement}
- */
+let friends = [
+    {
+        firstName: 'Artsiom',
+        lastName: 'Mezin'
+    },
+    {
+        firstName: 'Ilia',
+        lastName: 'Kantor'
+    },
+    {
+        firstName: 'Christopher',
+        lastName: 'Michael'
+    }
+]; 
+
 function makeFriendsList(friends) {
+let ul = document.createElement('ul');
+document.body.append(ul);
+
+return friends.map(friend => {
+let liAver = document.createElement('li');
+liAver.innerHTML = `$(friend.firstName) '/n' $(friend.lastName)`;
+return ul.append(liAver);
+})
+
 }
+makeFriendsList(friends);
+
+console.log(makeFriendsList(friends));
